4a11af9a71c94cc33151de64925ac1f807c6b8c6
Tamra Chan <tamrachan@gmail.com>
2025-09-03 12:44:12 +0100

    update: removed Edit Account from NavBar

--- front-end/src/components/NavBar/FlatPageNavBar.jsx
+++ front-end/src/components/NavBar/FlatPageNavBar.jsx
@@ -16,7 +16,7 @@ function FlatPageNavBar({ onSettingsClick }) {
         <div className="navbar-links">
             <Link to="/flatpage" className="nav-link">FlatPage</Link>
             <Link to="/games" className="nav-link">Mini games</Link>
-            <Link to="/details" className="nav-link">Edit account</Link>
+            {/* <Link to="/details" className="nav-link">Edit account</Link> */}
             <button onClick={onSettingsClick} className="settings-button">
                 <FaCog />
             </button>
